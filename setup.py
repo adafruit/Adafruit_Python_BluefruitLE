efaa472f4c97686e1162cacce25c52d77cefc756
Josh Bode <joshbode@fastmail.com>
2017-12-02 16:57:37 -0800

    fix UUIDs on python3/macOS and added pyobjc requirements for macOS

--- setup.py
+++ setup.py
@@ -1,6 +1,12 @@
 from ez_setup import use_setuptools
 use_setuptools()
 from setuptools import setup, find_packages
+import platform
+
+platform_install_requires = []
+
+if platform.system() == 'Darwin':
+    platform_install_requires += ['pyobjc-framework-CoreBluetooth']
 
 setup(name              = 'Adafruit_BluefruitLE',
       version           = '0.9.9',
@@ -9,5 +15,5 @@ setup(name              = 'Adafruit_BluefruitLE',
       description       = 'Python library for interacting with Bluefruit LE (Bluetooth low energy) devices on Linux or OSX.',
       license           = 'MIT',
       url               = 'https://github.com/adafruit/Adafruit_Python_BluefruitLE/',
-      install_requires  = ['future',],
+      install_requires  = ['future'] + platform_install_requires,
       packages          = find_packages())
