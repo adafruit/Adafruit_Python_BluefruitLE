d6abfea65fc530f693cad7b8ba41273f550085d6
Brennen Bearnes <bbearnes@gmail.com>
2018-05-23 09:48:44 -0600

    setup.py: rm ez_setup.py, add long_description from README.md, bump version

--- setup.py
+++ setup.py
@@ -1,12 +1,21 @@
-from ez_setup import use_setuptools
-use_setuptools()
 from setuptools import setup, find_packages
 
+# To use a consistent encoding
+from codecs import open
+from os import path
+
+here = path.abspath(path.dirname(__file__))
+
+# Get the long description from the README file
+with open(path.join(here, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
 setup(name              = 'Adafruit_BluefruitLE',
-      version           = '0.9.9',
+      version           = '0.9.10',
       author            = 'Tony DiCola',
       author_email      = 'tdicola@adafruit.com',
       description       = 'Python library for interacting with Bluefruit LE (Bluetooth low energy) devices on Linux or OSX.',
+      long_description  = long_description,
       license           = 'MIT',
       url               = 'https://github.com/adafruit/Adafruit_Python_BluefruitLE/',
       install_requires  = ['future',],
